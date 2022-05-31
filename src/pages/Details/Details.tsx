243107c24b33ace8079c4e323d3554f637e83834
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-05-31 09:42:02 +0300

    add pages

---
+++ src/pages/Details/Details.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import styles from './Details.module.css'
+
+interface DetailsProps {}
+
+const Details: FC<DetailsProps> = () => {
+  return (
+    <div className={styles.Details}>
+      
+    </div>
+  )
+}
+
+export { Details }
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import styles from './Home.module.css'
+
+interface HomeProps {}
+
+const Home: FC<HomeProps> = () => {
+  return (
+    <div className={styles.Home}>
+
+    </div>
+  )
+}
+
+export { Home }
